d95196140e2af4f5872816448041beb2e7e76356
Jeffrey Lederer <jeffrey@winnlederer.com>
2025-07-10 10:40:13 -0400

    usetable refactor

--- reacttype1.client/src/pages/League/Playoffs/List.tsx
+++ reacttype1.client/src/pages/League/Playoffs/List.tsx
@@ -1,22 +1,20 @@
 import { Link } from 'react-router-dom';
 import { UpdateFormData } from "@pages/League/Schedule/UpdateFormData.tsx";
 import LeagueClass from "@components/LeagueClass";
-//import UserClass from "@components/UserClass";
 import Layout from '@layouts/Layout.tsx';
 import { useQuery } from '@tanstack/react-query';
-//import { useMemo } from 'react';
+import { useMemo } from 'react';
 import { Spinner } from "flowbite-react";
-//import DisplayTable from '@components/DisplayTable';
-//import { ColumnDef } from '@tanstack/react-table';
+import DisplayTable from '@components/DisplayTable';
+import { ColumnDef } from '@tanstack/react-table';
 import fetchData from '@components/fetchData';
 import PlayoffGamesType from './PlayoffGamesType.tsx';
 
 function Playoffs() {
-    //const user = new UserClass();
+
     const league = new LeagueClass();
-    //const permission: string = user.role;
-    //const updateAllowed: boolean = (permission == "SiteAdmin" || permission == "Admin");
-   
+
+
 
     const { data, isLoading, error } = useQuery<UpdateFormData[]>({
         queryKey: ['schedules', league.id],
@@ -29,7 +27,7 @@ function Playoffs() {
         queryKey: ['PlayoffGames', league.id],
         queryFn: () => fetchData<UpdateFormData[]>(`/api/matches/PlayoffGames/${league.id}`),
         enabled: !!league.id,
-        
+
     });
 
     const HasMatches = (id: number): boolean => {
@@ -37,33 +35,34 @@ function Playoffs() {
 
     }
 
-    //const columns = useMemo<ColumnDef<UpdateFormData, unknown>[]>(() => [
-        
-    //    {
-    //        header: 'Game Date',
-    //        accessorKey: 'gameDate'
-    //    },
-    //    {
-    //        header: 'Cancelled',
-    //        accessorKey: 'cancelled'
-    //    },
-         
-    //    {
-    //        header: '',
-    //        id: 'actions',
-    //        cell: ({ row }) => (
-    //            <>
-    //                <span hidden={HasMatches(row.original.id) && !updateAllowed}>
-    //                    <Link to={'/league/playoffs/Create'} state={row.original.id.toString()} >Create Week's Teams</Link>
-    //                 </span>
-    //                <span hidden={!HasMatches(row.original.id) && !updateAllowed}>
-    //                    <Link to={'/league/playoffs/Create'} state={row.original.id.toString()} >Update Week's Teams</Link>|
-    //                    <Link to={`/League/playoffs/ListGames?id=${row.original.id}`} >Score Games</Link>
-    //                </span>
-    //            </>
-    //        ),
-    //    },
-    //], []);
+    const columns = useMemo<ColumnDef<PlayoffGamesType, unknown>[]>(() => [
+
+        {
+            header: 'Game Date',
+            accessorKey: 'gameDate'
+        },
+        {
+            header: 'Cancelled',
+            accessorKey: 'cancelled'
+        },
+
+        {
+            header: '',
+            id: 'actions',
+            cell: ({ row }) => (
+                <>
+                    <span hidden={HasMatches(row.original.id)} >
+                        <Link to={'/league/playoffs/Create'} state={row.original.id.toString()} >Create Week's Teams</Link>
+                     </span>
+                    <span hidden={!HasMatches(row.original.id)} >
+            <Link to={'/league/playoffs/Create'} state={row.original.id.toString()} >Update Week's Teams</Link> |
+            <Link to={`/League/playoffs/ListGames?id=${row.original.id}`}>Score Games</Link>
+                    </span >
+                </>
+            )
+        },
+
+    ], []);
 
     if (isLoading || isLoadingGames)
         return (
@@ -108,30 +107,7 @@ function Playoffs() {
             <Layout>
                 <h3 id="tableLabel">Playoff Schedule for League {league.leagueName}</h3>
 
-                <table>
-                    <thead>
-                        <th>Game Date</th>
-                        <th>Cancelled</th>
-                        <th></th>
-                    </thead>
-                    <tbody>
-                        {data?.map(item =>
-                            <tr>
-                                <td>{item.gameDate}</td>
-                                <td align="center">{item.cancelled ? "Yes" : "No"}</td>
-                                <td>
-                                    <span hidden={HasMatches(item.id)}>
-                                        <Link to={'/league/playoffs/Create'} state={item.id.toString()} >Create Week's Teams</Link>
-                                    </span>
-                                    <span hidden={!HasMatches(item.id)}>
-                                        <Link to={'/league/playoffs/Update'} state={item.id.toString()} >Update Week's Teams</Link> |&nbsp;
-                                        <Link to={`/League/playoffs/ListGames?id=${item.id}`} >Score Games</Link> 
-                                    </span>
-                                </td>
-                            </tr>
-                        )}
-                    </tbody>
-                </table>
+                <DisplayTable<UpdateFormData> data={data} columns={columns} />
             </Layout>
         );
     }
